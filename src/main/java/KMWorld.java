b0a5fdafbbfb78a7f66d5ed072ee12302b4620f3
Kevin Mahoney <kmahoney@forwardslope.com>
2016-12-29 08:41:04 -0800

    set up for merge

--- src/main/java/KMWorld.java
+++ src/main/java/KMWorld.java
@@ -1,6 +1,6 @@
 public class KMWorld{
 	public static void main (String args[]){
 		System.out.println("Hello KM");
-		System.out.println("km_dev branch");
+		System.out.println("km_dev branch, for merge");
 	}
 }
